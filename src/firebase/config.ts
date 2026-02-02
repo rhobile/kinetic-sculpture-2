32174eff8531cf26167cc598117b9295a5602237
Andrew Jones <rhobile@gmail.com>
2026-02-02 10:25:35 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,9 @@
-// This file will be populated with your Firebase project configuration.
-// It is intentionally left empty for now and will be updated by the system.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  projectId: "kinetic-sculptures-2-936-80a86",
+  appId: "1:792239010124:web:b59f3d9e95aacdf5ba5fe9",
+  apiKey: "AIzaSyDZOCX2AFUa4YqUsAKvIMH9F71UhbH0beo",
+  authDomain: "kinetic-sculptures-2-936-80a86.firebaseapp.com",
+  storageBucket: "kinetic-sculptures-2-936-80a86.appspot.com",
+  measurementId: "",
+  messagingSenderId: "792239010124"
+};
