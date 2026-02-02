6e6e1d1abb7df0d5dda61163c8168f0b4f5cbb96
Andrew Jones <rhobile@gmail.com>
2026-02-02 11:50:48 +0000

    please link this project kinetic-sculptures-2-93686883 to my cloud stora

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  projectId: "kinetic-sculptures-2-936-80a86",
+  projectId: "kinetic-sculptures-2-93686883",
   appId: "1:792239010124:web:b59f3d9e95aacdf5ba5fe9",
   apiKey: "AIzaSyDZOCX2AFUa4YqUsAKvIMH9F71UhbH0beo",
-  authDomain: "kinetic-sculptures-2-936-80a86.firebaseapp.com",
-  storageBucket: "kinetic-sculptures-2-936-80a86.appspot.com",
+  authDomain: "kinetic-sculptures-2-93686883.firebaseapp.com",
+  storageBucket: "rhobile2027.firebasestorage.app",
   measurementId: "",
   messagingSenderId: "792239010124"
 };
