6830e9d278de68c48f8d8ff56d2b6a1f66a52e7e
Andrew Jones <rhobile@gmail.com>
2026-02-04 10:39:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:792239010124:web:b59f3d9e95aacdf5ba5fe9",
   "apiKey": "AIzaSyDZOCX2AFUa4YqUsAKvIMH9F71UhbH0beo",
   "authDomain": "kinetic-sculptures-2-936-80a86.firebaseapp.com",
+  "storageBucket": "kinetic-sculptures-2-936-80a86.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "792239010124"
 };
